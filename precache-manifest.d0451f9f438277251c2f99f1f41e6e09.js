64f566c396f5b9fc3f12065915650d8f33131f41
khamerling-potts <54592576+khamerling-potts@users.noreply.github.com>
2020-12-10 10:05:33 -0600

    Updates

---
+++ precache-manifest.d0451f9f438277251c2f99f1f41e6e09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8a19f23993ac3630a9",
+    "url": "/09-todo-react-khamerling-potts/static/css/main.b037fdd1.chunk.css"
+  },
+  {
+    "revision": "9c8a19f23993ac3630a9",
+    "url": "/09-todo-react-khamerling-potts/static/js/main.9c8a19f2.chunk.js"
+  },
+  {
+    "revision": "773fedcca2e46286e9a3",
+    "url": "/09-todo-react-khamerling-potts/static/js/1.773fedcc.chunk.js"
+  },
+  {
+    "revision": "5e376a1e4057d41e8ccd",
+    "url": "/09-todo-react-khamerling-potts/static/js/runtime~main.5e376a1e.js"
+  },
+  {
+    "revision": "e0b674b4f15b1dca283422533b919362",
+    "url": "/09-todo-react-khamerling-potts/index.html"
+  }
+];
